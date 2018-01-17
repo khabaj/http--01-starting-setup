20fc504a96acf942c1c836267d5d08729c9c0d9e
Krystian Habaj <khabaj@pgs-soft.com>
2018-01-17 13:44:28 +0100

    147. Rendering Fetched Data to the Screen

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,20 +8,27 @@ import './Blog.css';
 
 class Blog extends Component {
 
-    componentDidMount() {
-        axios.get('http://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-            console.log(response);
-        });
+    state = {
+        posts: []
     }
 
-    render () {
+    componentDidMount() {
+        axios.get('http://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.setState({ posts: response.data });
+                console.log(response);
+            });
+    }
+
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title}/>
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
