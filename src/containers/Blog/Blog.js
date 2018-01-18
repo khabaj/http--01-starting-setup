3f761d51db88fcd61af5e0c5172266524e892bf0
Krystian Habaj <khabaj@pgs-soft.com>
2018-01-18 12:40:17 +0100

    192. Creating Dynamic Nested Routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from '../../axios';
 import Posts from './Posts/Posts'
 import './Blog.css';
 import { Route, NavLink, Switch } from 'react-router-dom';
